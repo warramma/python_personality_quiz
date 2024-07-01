472d105ad23c57499287d96d7ef57b43d8c09575
warramma <wishmorethewitch@gmail.com>
2024-07-01 09:31:30 -0500

    adjust Question object for personality quiz

--- question.py
+++ question.py
@@ -2,23 +2,11 @@ class Question:
     a_score = 0
     b_score = 0
 
-    def __init__ (self, prompt, answer):
+    def __init__ (self, prompt, black_cat, golden_retriever):
         self.prompt = prompt
-        self.answer = answer
+        self.black_cat = black_cat
+        self.golden_retriever = golden_retriever
     
     def display_question(self):
-        print(self)
-
-    def calculate_result(self):
-        if(self.answer.lower() == 'a'):
-            a_score+=1
-        elif(self.answer.lower()=='b'):
-            b_score+=1
-        else:
-            print('\ninvalid input...')
-        if(a_score > b_score):
-            print('Black cat!')
-        elif(a_score < b_score):
-            print('Golden retriever!')
-        else:
-            print('You\'re a perfect mix!')
+        return print(self.prompt)
+    
--- quiz.py
+++ quiz.py
@@ -1,1 +1,25 @@
 #6/26/2024
+
+#import Question class
+from Question import Question
+
+#create array of questions
+questions = [
+    'On an ideal weekend, would you prefer: \n a) cozy day home doing hobbies, chilling, etc \nb) Fun day out with friends, time outside', 
+    'In parties, are you usually: \na) Staying mostly in one spot, talking mostly to close friends, might be having fun but also wanting to go home \nb) Meeting as many people as possible, making new friends, having a blast'
+]
+#print(questions)
+
+#create question objects in dictionary
+quiz = {
+    '1' : Question(questions[0], 'a', 'b'),
+    '2' : Question(questions[1], 'a', 'b')
+}
+
+#create score variables
+a_score = 0
+b_score = 0
+
+#loop through questions and ask which you prefer, keeping score in the right variable
+for question in quiz:
+    quiz[question].display_question
