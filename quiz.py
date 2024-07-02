e06d06e572a44195ce4c0f0993abb179e5e337c5
warramma <wishmorethewitch@gmail.com>
2024-07-02 16:28:47 -0500

    Track score and print results

--- quiz.py
+++ quiz.py
@@ -1,6 +1,5 @@
 #6/26/2024
 
-#import Question class
 from Question import Question
 
 #create array of questions
@@ -8,7 +7,7 @@ questions = [
     'On an ideal weekend, would you prefer: \na) cozy day home doing hobbies, chilling, etc \nb) Fun day out with friends, time outside', 
     'In parties, are you usually: \na) Staying mostly in one spot, talking mostly to close friends, might be having fun but also wanting to go home \nb) Meeting as many people as possible, making new friends, having a blast'
 ]
-#print(questions)
+#print(questions) (just testing hehe)
 
 #create question objects in dictionary
 quiz = {
@@ -20,7 +19,27 @@ quiz = {
 a_score = 0
 b_score = 0
 
+#welcome user + disclaimer
+print('Welcome to Black Cat vs Golden Retriever personality test!\nDisclaimer: this test is just for run and is not necessarily an accurate depiction of who you actually are :)\nLet\'s get started!\n')
+
 #loop through questions and ask which you prefer, keeping score in the right variable
 for question in quiz:
-    answer = quiz[question].ask_question()
-    print('You answered ' + str(answer) + '! Woohoo!')
+    answer = str(quiz[question].ask_question())
+    #print('You answered ' + str(answer) + '! Woohoo!')
+    if(answer.lower() == quiz[question].black_cat):
+        a_score +=1
+    elif(answer.lower() == quiz[question].golden_retriever):
+        b_score +=1
+    else:
+        print('invalid input...')
+
+print('calculating your results......\n...............................\n......................')
+
+#results
+if(a_score > b_score):
+    print('You are a black cat! 🐈‍⬛\nYou are introverted and deeply mysterious, but deeply affectionate around those you love! \nYou love to stay in and cuddle, read books, and have plenty of alone time!\nYou safeguard your energy dearly because it gets easily depleted!\nYou dislike suprises and loud noises and welcome routine!\nWelcome to the club!!')
+elif(b_score > a_score):
+    print('You are a golden retriever!\nExtroverted and very friendly, you really get around!\nYou are always making new friends!\nYou are a great listener and get along with everyone!\nYou love spending time with loved ones and being active! 🐕')
+else:
+    print('You are equal parts black cat 🐈‍⬛ and golden retriever 🐕!! Way to forge your own path :)')
+    print('You straddle the line between introversion and extroversion. Although you love people, you also love you time! \nYou are passionate about your hobbies and love making news friends and experiencing new things!')
